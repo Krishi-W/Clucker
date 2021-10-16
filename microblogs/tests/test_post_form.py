7fa71597caa8619266b86b2ba96ea2bece4b06b5
Krishi Wali <krishi.wali@kcl.ac.uk>
2021-10-16 22:38:37 +0100

    Add tests for PostForm

---
+++ microblogs/tests/test_post_form.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from microblogs.forms import PostForm
+
+class PostFormTestCase(TestCase):
+    def setUp(self):
+        self.form_input = {
+            "text": "This is the first Cluck!"
+        }
+
+    def test_valid_post_form(self):
+        form = PostForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_form_has_necessary_fields(self):
+        form = PostForm()
+        self.assertIn("text", form.fields)
+
+    def test_form_uses_model_validation(self):
+        form = PostForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+        self.form_input["text"] = "x" * 281
+        form = PostForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+        self.form_input["text"] = ""
+        form = PostForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
